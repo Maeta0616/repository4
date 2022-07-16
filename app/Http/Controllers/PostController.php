e88580f339bf236866767c2848384c4e11f7d8b2
Maeta0616 <maeryo616@gmail.com>
2022-07-16 13:59:17 +0900

    add[記事詳細画面]

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,12 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //post一覧を表示する//
-    public function index(Post $post)
-{
-    return $post->get();
-}
+     public function index(Post $post)
+      {
+       return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+      }
+      public function show(Post $post)
+      {
+        return view('posts/show')->with(['post'=>$post]);
+      }
 }
